fd1bce8668092d4309908d44dee9e59abfd27b92
Emmanuel Krebs <e-krebs@users.noreply.github.com>
2024-03-25 10:05:24 +0100

    fix: snapping + better responsiveness

--- src/pages/Me.tsx
+++ src/pages/Me.tsx
@@ -10,15 +10,14 @@ import {
 } from "react-icons/md";
 import { CardBack, CardFront } from "../PlayingCard";
 import { NavLink, useNavigate, useParams } from "react-router-dom";
-import { FC, useEffect, useReducer, useRef } from "react";
+import { FC, useEffect, useMemo, useReducer, useRef } from "react";
 
 const person = new URL("../img/person.svg", import.meta.url);
 const linkedin = new URL("../img/linkedin.svg", import.meta.url);
 const twitter = new URL("../img/twitter.svg", import.meta.url);
 const github = new URL("../img/github.svg", import.meta.url);
 
-const snapWidth = 382;
-const max = 4;
+const safeIndex = (index: number) => Math.min(4, Math.max(0, index));
 
 const SnapButton: FC<{
   position: "left" | "right";
@@ -44,134 +43,175 @@ const SnapButton: FC<{
           position === "right" && "right-3 hover:translate-x-1"
         )}
         onClick={() => {
-          navigate(
-            `/home/${Math.min(max, Math.max(0, parseInt(index) + (position === "left" ? -1 : 1)))}`
-          );
+          navigate(`/home/${safeIndex(parseInt(index) + (position === "left" ? -1 : 1))}`);
         }}
       >
-        {position === "left" && <MdArrowBack />}
-        {position === "right" && <MdArrowForward />}
+        {position === "left" && <MdArrowBack className="w-4 h-4 md:w-8 md:h-8" />}
+        {position === "right" && <MdArrowForward className="w-4 h-4 md:w-8 md:h-8" />}
       </button>
     </>
   );
 };
+
+const Spacer = () => (
+  <div className="w-[calc(50%-10rem-1.5rem)] md:w-[calc(50%-12rem-1.5rem)] shrink-0" />
+);
+
 export const Me = () => {
-  const ref = useRef<HTMLDivElement>(null);
+  const navigate = useNavigate();
+  const scrollerRef = useRef<HTMLDivElement>(null);
+  const cardRef = useRef<HTMLDivElement>(null);
 
   const { index } = useParams<{ index: string }>();
 
+  const snapWidth = useMemo(
+    () => cardRef.current?.offsetWidth + 24, // gap-6
+    [scrollerRef.current?.offsetWidth]
+  );
+
   useEffect(() => {
-    ref.current.scrollTo({ left: snapWidth * parseInt(index), behavior: "smooth" });
+    const left = snapWidth * parseInt(index);
+    console.log({ left }, scrollerRef.current.scrollLeft);
+    scrollerRef.current.scrollTo({ left: left, behavior: "smooth" });
   }, [index]);
 
+  useEffect(() => {
+    const scrollEvent = () => {
+      const currentIndex = Math.floor(scrollerRef.current.scrollLeft / snapWidth);
+      if (index !== `${currentIndex}`) {
+        navigate(`/home/${safeIndex(currentIndex)}`);
+      }
+    };
+    scrollerRef.current?.addEventListener("scrollend", scrollEvent);
+
+    return () => {
+      scrollerRef.current?.removeEventListener("scrollend", scrollEvent);
+    };
+  }, [scrollerRef.current]);
+
   return (
-    <div
-      ref={ref}
-      className="relative h-full w-full flex gap-3 md:gap-6 snap-x *:snap-center *:my-auto overflow-x-auto"
-    >
+    <>
       <SnapButton position="left" />
       <SnapButton position="right" />
-      <div className="w-[calc(50%-12rem-1.5rem)] shrink-0" />
-      <CardFront icon={MdPerson} className="w-96">
-        <div className="absolute top-12 w-full flex flex-col gap-y-6">
-          <img src={person.href} alt="person" className="mx-auto max-h-64" />
-          <title className="text-4xl font-semibold capitalize flex flex-col w-full items-center p-3">
-            <h1>Emmanuel Krebs</h1>
-            <span className="text-xl">Senior software engineer</span>
-          </title>
-          <div className="flex gap-x-6 justify-center text-white">
-            <a
-              href="https://www.linkedin.com/in/emmanuel-krebs-37926b91/"
-              title="linkedin"
-              target="_blank"
-            >
-              <img src={linkedin.href} alt="linkedin" className="w-9 h-9" />
-            </a>
-            <a href="https://twitter.com/emmkrebs" title="twitter" target="_blank">
-              <img src={twitter.href} alt="twitter" className="w-9 h-9" />
-            </a>
-            <a href="https://github.com/e-krebs" title="github" target="_blank">
-              <img src={github.href} alt="github" className="w-9 h-9" />
-            </a>
-          </div>
-        </div>
-      </CardFront>
-      <CardFront className="w-96" icon={MdCalendarMonth}>
-        <div className="absolute top-12 w-full flex flex-col gap-y-6">
-          <title className="text-4xl font-semibold capitalize flex w-full justify-center p-3">
-            Career & Training
-          </title>
-          <ul className="pl-12 pr-6 space-y-3 text-xl list-outside list-disc">
-            <li>
-              <a href="http://www.sudoc.fr/132355760" target="_blank" className="self-center">
-                <b>2008:</b> PhD in theoretical chemistry at Paris 6 university and IFP{" "}
-                <MdOpenInNew className="inline w-4 h-4" />
+
+      <div
+        ref={scrollerRef}
+        className="relative h-full w-full flex gap-6 snap-x *:snap-center *:my-auto overflow-x-auto"
+      >
+        <Spacer />
+
+        <CardFront icon={MdPerson} className="w-80 md:w-96" ref={cardRef}>
+          <div className="absolute top-12 w-full flex flex-col gap-y-6">
+            <img src={person.href} alt="person" className="mx-auto max-h-56 md:max-h-64" />
+            <title className="text-3xl md:text-4xl font-semibold capitalize flex flex-col w-full items-center p-3">
+              <h1>Emmanuel Krebs</h1>
+              <span className="text-lg md:text-xl">Senior software engineer</span>
+            </title>
+            <div className="flex gap-x-6 justify-center text-white">
+              <a
+                href="https://www.linkedin.com/in/emmanuel-krebs-37926b91/"
+                title="linkedin"
+                target="_blank"
+              >
+                <img src={linkedin.href} alt="linkedin" className="w-9 h-9" />
               </a>
-            </li>
-            <li>
-              <b>2012:</b> started as a software developer at expertime (a service company)
-            </li>
-            <li>
-              <b>2014:</b> became a technical consultant at expertime
-            </li>
-            <li>
-              <b>2017:</b> became a software architect at expertime
-            </li>
-            <li>
-              <b>2019:</b> became a senior software engineer at algolia
-            </li>
-          </ul>
-        </div>
-      </CardFront>
-      <CardFront className="w-96" icon={MdCode}>
-        <div className="absolute top-12 w-full flex flex-col gap-y-6">
-          <title className="text-4xl font-semibold capitalize flex w-full justify-center p-3">
-            Skills
-          </title>
-          <ul className="pl-12 pr-6 space-y-3 text-xl list-outside list-disc">
-            <li>React, Typescript & more</li>
-            <li>DevX is very important to me</li>
-            <li>I have a taste for UI and UX</li>
-            <li>most of my professional work is closed-source</li>
-            <li>I'm a problem solver</li>
-            <li>I'm a fast learner</li>
-            <li>I'm a very organised person, tending to always anticipate what's to come</li>
-          </ul>
-        </div>
-      </CardFront>
-      <CardFront className="w-96" icon={MdMoreHoriz}>
-        <div className="absolute top-12 w-full flex flex-col gap-y-6">
-          <title className="text-4xl font-semibold capitalize flex w-full justify-center p-3">
-            ... and more
-          </title>
-          <ul className="pl-12 pr-6 space-y-3 text-xl list-outside list-disc">
-            <li>native french, professional english</li>
-            <li>I live in Paris (France)</li>
-            <li>I'm an enthusiactic person who likes team work & challenges</li>
-            <li>I am used to (and enjoy) tutoring junior people</li>
-            <li>I'm a curious person, always looking at what's happening around</li>
-            <li>I'm not looking for a new work experience, at least right now!</li>
-          </ul>
-        </div>
-      </CardFront>
-      <CardBack className="w-96 hover:rotate-1" icon={MdArrowForward}>
-        <div className="absolute w-full h-full px-6 flex items-center justify-center">
-          <div className="backdrop-blur-sm text-2xl leading-10 text-center">
-            more details in the{" "}
-            <NavLink className={"underline"} to="/work">
-              Work Experiences
-              <MdOpenInNew className="ml-2 inline w-4 h-4" />
-            </NavLink>{" "}
-            and{" "}
-            <NavLink className={"underline"} to="/other">
-              Other Projects
-              <MdOpenInNew className="ml-2 inline w-4 h-4" />
-            </NavLink>{" "}
-            sections
+              <a href="https://twitter.com/emmkrebs" title="twitter" target="_blank">
+                <img src={twitter.href} alt="twitter" className="w-9 h-9" />
+              </a>
+              <a href="https://github.com/e-krebs" title="github" target="_blank">
+                <img src={github.href} alt="github" className="w-9 h-9" />
+              </a>
+            </div>
           </div>
-        </div>
-      </CardBack>
-      <div className="w-[calc(50%-12rem-1.5rem)] shrink-0" />
-    </div>
+        </CardFront>
+
+        <CardFront className="w-80 md:w-96" icon={MdCalendarMonth}>
+          <div className="absolute top-9 md:top-12 w-full flex flex-col gap-y-3 md:gap-y-6">
+            <title className="text-3xl md:text-4xl font-semibold capitalize flex w-full justify-center p-3">
+              Career & Training
+            </title>
+            <ul className="pl-9 pr-3 md:pl-12 md:pr-6 space-y-3 text-lg md:text-xl list-outside list-disc">
+              <li>
+                <b>2008:</b>{" "}
+                <a href="http://www.sudoc.fr/132355760" target="_blank" className="underline">
+                  PhD <MdOpenInNew className="inline w-4 h-4" />
+                </a>{" "}
+                in theoretical chemistry at UPMC and IFP
+              </li>
+              <li>
+                <b>2012:</b> started as a software developer at expertime (a service company)
+              </li>
+              <li>
+                <b>2014:</b> became a technical consultant at expertime
+              </li>
+              <li>
+                <b>2017:</b> became a software architect at expertime
+              </li>
+              <li>
+                <b>2019:</b> became a senior software engineer at algolia
+              </li>
+            </ul>
+          </div>
+        </CardFront>
+
+        <CardFront className="w-80 md:w-96" icon={MdCode}>
+          <div className="absolute top-9 md:top-12 w-full flex flex-col gap-y-3 md:gap-y-6">
+            <title className="text-3xl md:text-4xl font-semibold capitalize flex w-full justify-center p-3">
+              Skills
+            </title>
+            <ul className="pl-9 pr-3 md:pl-12 md:pr-6 space-y-3 text-lg md:text-xl list-outside list-disc">
+              <li>React, Typescript & more</li>
+              <li>DevX is very important to me</li>
+              <li>I have a taste for UI and UX</li>
+              <li>Most of my professional work is closed-source</li>
+              <li>I'm a problem solver</li>
+              <li>I'm a fast learner</li>
+              <li>I'm a very organised person, tending to always anticipate what's to come</li>
+            </ul>
+          </div>
+        </CardFront>
+
+        <CardFront className="w-80 md:w-96" icon={MdMoreHoriz}>
+          <div className="absolute top-9 md:top-12 w-full flex flex-col gap-y-3 md:gap-y-6">
+            <title className="text-3xl md:text-4xl font-semibold capitalize flex w-full justify-center p-3">
+              ... and more
+            </title>
+            <ul className="pl-9 pr-3 md:pl-12 md:pr-6 space-y-1.5 md:space-y-3 text-lg md:text-xl list-outside list-disc">
+              <li>Native french, professional english</li>
+              <li>I live in Paris (France)</li>
+              <li>I'm an enthusiactic person who likes team work & challenges</li>
+              <li>I am used to (and enjoy) tutoring junior people</li>
+              <li>I'm a curious person, always looking at what's happening around</li>
+              <li>I'm not looking for a new work experience, at least right now!</li>
+            </ul>
+          </div>
+        </CardFront>
+
+        <CardBack
+          className={cx(
+            "w-80 md:w-96 hover:rotate-1",
+            index === "4" && "delay-300 md:delay-0 rotate-1 md:rotate-0"
+          )}
+          icon={MdArrowForward}
+        >
+          <div className="absolute w-full h-full px-6 flex items-center justify-center">
+            <div className="backdrop-blur-sm text-2xl leading-10 text-center">
+              more details in the{" "}
+              <NavLink className={"underline"} to="/work">
+                Work Experiences
+                <MdOpenInNew className="ml-2 inline w-4 h-4" />
+              </NavLink>{" "}
+              and{" "}
+              <NavLink className={"underline"} to="/other">
+                Other Projects
+                <MdOpenInNew className="ml-2 inline w-4 h-4" />
+              </NavLink>{" "}
+              sections
+            </div>
+          </div>
+        </CardBack>
+        <Spacer />
+      </div>
+    </>
   );
 };
