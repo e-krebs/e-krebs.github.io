b770517e1091df8524ba141b63adec1991ced8e1
Emmanuel Krebs <e-krebs@users.noreply.github.com>
2024-03-25 21:18:48 +0100

    tentative fix

--- src/Shell.tsx
+++ src/Shell.tsx
@@ -1,7 +1,7 @@
 import cx from "clsx";
 import { NavLink, Outlet } from "react-router-dom";
 import { CardBack, CardBackProps } from "./PlayingCard";
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import { MdPerson, MdWork, MdAllInclusive } from "react-icons/md";
 
 interface CardLinkProps extends CardBackProps {
@@ -25,32 +25,45 @@ const CardLink: FC<CardLinkProps> = ({ to, title, icon, className }) => (
   </NavLink>
 );
 
-export const Shell = () => (
-  <div className="flex flex-col">
-    <header className="relative h-32">
-      <div className="h-full md:max-w-[50rem] mx-auto grid grid-cols-3 justify-end px-2 md:px-6 -mt-[20.5rem] md:-mt-64 gap-6">
-        <CardLink
-          to="/home"
-          title="who am I?"
-          icon={MdPerson}
-          className="z-30 hover:z-40 max-w-[50vw] md:max-w-96 bottom-0 left-0"
-        />
-        <CardLink
-          to="/work"
-          title="work experiences"
-          icon={MdWork}
-          className="z-20 max-w-[50vw] md:max-w-96 bottom-0 left-[25vw] justify-center"
-        />
-        <CardLink
-          to="/other"
-          title="other projects"
-          icon={MdAllInclusive}
-          className="z-10 max-w-[50vw] md:max-w-96 bottom-0 right-0 justify-end"
-        />
-      </div>
-    </header>
-    <main className="relative flex justify-center items-center w-full h-[calc(100vh-8rem)] overflow-hidden">
-      <Outlet />
-    </main>
-  </div>
-);
+export const Shell = () => {
+  useEffect(() => {
+    const setSize = () => {
+      document.documentElement.style.setProperty("--vw", `${window.innerWidth / 100}px`);
+    };
+    setSize();
+    window.addEventListener("resize", setSize);
+    return () => {
+      window.removeEventListener("resize", setSize);
+    };
+  }, []);
+
+  return (
+    <div className="flex flex-col">
+      <header className="relative h-32">
+        <div className="h-full md:max-w-[50rem] mx-auto grid grid-cols-3 justify-end px-2 md:px-6 -mt-[20.5rem] md:-mt-64 gap-6">
+          <CardLink
+            to="/home"
+            title="who am I?"
+            icon={MdPerson}
+            className="z-30 hover:z-40 max-w-[50vw] md:max-w-96 bottom-0 left-0"
+          />
+          <CardLink
+            to="/work"
+            title="work experiences"
+            icon={MdWork}
+            className="z-20 max-w-[50vw] md:max-w-96 bottom-0 left-[25vw] justify-center"
+          />
+          <CardLink
+            to="/other"
+            title="other projects"
+            icon={MdAllInclusive}
+            className="z-10 max-w-[50vw] md:max-w-96 bottom-0 right-0 justify-end"
+          />
+        </div>
+      </header>
+      <main className="relative flex justify-center items-center w-full h-[calc(100vh-8rem)] overflow-hidden">
+        <Outlet />
+      </main>
+    </div>
+  );
+};
